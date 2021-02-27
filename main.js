059a48ef9afa2743ac0080163bebb2399e8a933c
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-02-27 23:09:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("katega")
+const length = document.getElementById("length");
+const number = document.getElementById("number");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const copy = document.getElementById("copy");
+const generate = document.getElementById("generate");
