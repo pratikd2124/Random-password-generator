3122434901fb24d94fdaf49101e971f444517bc0
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-02-28 18:18:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const length = document.getElementById("length");
-const number = document.getElementById("number");
-const uppercase = document.getElementById("uppercase");
-const lowercase = document.getElementById("lowercase");
-const copy = document.getElementById("copy");
-const generate = document.getElementById("generate");
+        const number = document.getElementById("number");
+        const char = document.getElementById("char");
+        const uppercase = document.getElementById("uppercase");
+        const lowercase = document.getElementById("lowercase");
+        
+        const passcode = document.getElementById("passcode");
+        const grt = document.getElementById("generate");
+
+        const cpy = document.getElementById("copy");
+
+
+
+        const randomfnc = {
+            lower : getrandomlower(),
+            upper : getrandomupper(),
+            numbers : getrandomnumber(),
+            characters : getrandomchar()
+        }
+        
+        function getrandomlower(){
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        }
+        function getrandomupper(){
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        }
+        function getrandomnumber(){
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+        }
+        function getrandomchar(){
+            const symbols = "!@#$%^&*(){}[]=<>/."
+            return symbols[Math.floor(Math.random() * symbols.length)];
+        }
+        
