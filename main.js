2e5ac5dfc9e216e43bda49c6183a68a143424e4f
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-03-01 17:50:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const length = document.getElementById("length");
+        const lengthy = document.getElementById("length");
         const number = document.getElementById("number");
         const char = document.getElementById("char");
         const uppercase = document.getElementById("uppercase");
@@ -12,12 +12,59 @@ const length = document.getElementById("length");
 
 
         const randomfnc = {
-            lower : getrandomlower(),
-            upper : getrandomupper(),
-            numbers : getrandomnumber(),
-            characters : getrandomchar()
+            lower : getrandomlower,
+            upper : getrandomupper,
+            characters : getrandomchar,
+            numbers : getrandomnumber
+            
         }
+
         
+        cpy.addEventListener("click", () => {
+            const textarea = document.createElement("textarea");
+            const password = passcode.innerText;
+
+            if (!password) {alert ("NO pass generated");}
+
+            else{
+            textarea.value = password;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand("copy");
+            textarea.remove();
+            alert('Generated password copied to clipboard');
+            }
+        });
+
+        grt.addEventListener("click", function(){
+            const l = +lengthy.value;
+            const n = number.checked;
+            const c = char.checked;
+            const uc = uppercase.checked;
+            const lc = lowercase.checked;
+
+            document.querySelector("#passcode").innerText = generatepass(lc, uc, c, n, l);
+        });
+
+        function generatepass(lower,upper,characters,numbers,lengthy){
+            let finalpassword = '';
+            const randompasscode = lower + upper + characters + numbers;
+            const randomArray = [{lower}, {upper}, {characters}, {numbers}].filter(item => Object.values(item)[0]);
+
+            if (randompasscode === 0){
+                return '';
+            }
+            else{
+            for (let i = 0; i < lengthy; i+=randompasscode){
+                randomArray.forEach(type => {
+                    const funcName = Object.keys(type)[0];
+                    finalpassword += randomfnc[funcName]();
+                });
+            }
+
+            return finalpassword;}
+        }
+
         function getrandomlower(){
             return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
         }
